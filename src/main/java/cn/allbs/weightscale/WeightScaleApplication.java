a0e87b5eeed0985bde39f2047ebb22053d5f931d
chenqi <chenqi92104@icloud.com>
2024-06-11 11:19:03 +0800

    init

---
+++ src/main/java/cn/allbs/weightscale/WeightScaleApplication.java
@@ -0,0 +1,13 @@
+package cn.allbs.weightscale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightScaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightScaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/allbs/weightscale/WeightScaleApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.allbs.weightscale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeightScaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
